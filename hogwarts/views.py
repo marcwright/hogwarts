ee6664a7a7d40adb79c67286d520bdf870675ab6
Marc Wright <marc@generalassemb.ly>
2020-04-02 11:29:02 -0500

    Finihed full crud for students

--- hogwarts/views.py
+++ hogwarts/views.py
@@ -3,21 +3,14 @@ from django.shortcuts import render, redirect
 from .forms import HouseForm, StudentForm
 from django.urls import reverse
 
-# https: // stackoverflow.com/questions/45135263/class-has-no-objects-member
-
-# INSERT INTO hogwarts_house(name, image_url) VALUES('marc', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Hogwarts_Castle_%2843281949792%29.jpg/1200px-Hogwarts_Castle_%2843281949792%29.jpg')
-
-# 'bytes' no attribute 'get' error
-# https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
-
 def house_list(request):
     houses = House.objects.all()
     # print(houses)
     print(request)
     return render(request, 'house_list.html', {'houses': houses})
 
-def house_detail(request, id):
-    house = House.objects.get(id = id)
+def house_detail(request, pk):
+    house = House.objects.get(pk=id)
     return res(request, 'house_detail.html', {'house': house})
 
 def house_create(request):
@@ -30,8 +23,8 @@ def house_create(request):
         form = HouseForm()
         return render(request, 'house_form.html', {'form': form})
 
-def house_update(request, id):
-    house = House.objects.get(id = id)
+def house_update(request, pk):
+    house = House.objects.get(pk=id)
     if request.method == 'POST':
         form = HouseForm(request.body, instance = house)
         if form.is_valid:
@@ -67,18 +60,29 @@ def student_create(request):
         form = StudentForm()
         return render(request, 'student_form.html', {'form': form})
 
-def student_update(request, id):
-    student = Student.objects.get(id = id)
+def student_update(request, pk):
+    student = Student.objects.get(pk=pk)
     if request.method == 'POST':
-        form = StudentForm(request.body, instance = student)
+        form = StudentForm(request.POST, instance=student)
         if form.is_valid:
             student = form.save()
-            return redirect('student_detail', id = student.id)
+            return redirect(reverse('student_detail', args=(student.id,)))
     else:
         form = StudentForm(instance = student)
         return render(request, 'student_form.html', {'form': form})
 
-def student_delete(request, id):
+def student_delete(request, pk):
     if request.method == 'POST':
-        Student.objects.get(id = id).delete()
+        Student.objects.get(pk=pk).delete()
     return redirect('student_list')
+
+
+# https: // stackoverflow.com/questions/45135263/class-has-no-objects-member
+
+# INSERT INTO hogwarts_house(name, image_url) VALUES('marc', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Hogwarts_Castle_%2843281949792%29.jpg/1200px-Hogwarts_Castle_%2843281949792%29.jpg')
+
+# 'bytes' no attribute 'get' error
+# https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
+
+# _reverse_with_prefix() argument after * must be an iterable, not int
+# https: // stackoverflow.com/questions/52575418/reverse-with-prefix-argument-after-must-be-an-iterable-not-int
